28df210c6c02ca7bf6529f0d4d32213bd2ef8f49
Aurélie GRAMUSSET <gramusset.a@gmail.com>
2024-10-27 22:59:14 +0100

    Add PHP code quality

--- api/.php-cs-fixer.dist.php
+++ api/.php-cs-fixer.dist.php
@@ -2,12 +2,18 @@
 
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
-    ->exclude('var')
+    ->exclude(['var', 'vendor'])
 ;
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'blank_line_after_opening_tag' => false,
+        'declare_strict_types' => true,
+        'global_namespace_import' => false,
+        'no_superfluous_phpdoc_tags' => ['remove_inheritdoc' => false],
     ])
+    ->setUsingCache(false)
     ->setFinder($finder)
 ;
