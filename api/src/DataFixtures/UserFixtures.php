040ac891a3dda71878b91423a4282f53e029d97a
Aurélie GRAMUSSET <gramusset.a@gmail.com>
2025-01-24 15:04:01 +0100

    add role admin and employee

---
+++ api/src/DataFixtures/UserFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\DataFixtures\abstraction\AbstractTsvImport;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends AbstractTsvImport
+{
+    private const FILENAME = 'user.tsv';
+
+    public function __construct(string $rootDirectory, private readonly UserPasswordHasherInterface $userPasswordHasher) {
+        parent::__construct($rootDirectory);
+    }
+
+    public function getTsvFilename(): string
+    {
+        return self::FILENAME;
+    }
+
+    public function skipEmptyLine(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @param array $element
+     */
+    public function insertItem(EntityManagerInterface $manager, Generator $faker, array $element): bool
+    {
+        $user = (new User())
+            ->setFirstName($element[0])
+            ->setLastName($element[1])
+            ->setEmail($element[2])
+            ->setRoles([$element[4]]);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, $element[3]));
+
+        $this->addReference('user_'.$element[2], $user);
+        $manager->persist($user);
+
+        return true;
+    }
+}
