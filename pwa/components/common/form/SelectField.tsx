2e75674a2d347f7afa29fab964c724e458bdd9b1
Aurélie GRAMUSSET <gramusset.a@gmail.com>
2025-01-31 19:02:15 +0100

    Create autocomplete and inputTimeField components Modify inputField and selectField components

--- pwa/components/common/form/SelectField.tsx
+++ pwa/components/common/form/SelectField.tsx
@@ -1,12 +1,23 @@
 import {Option} from "./utils";
 import React from "react";
 
-const SelectField = ({register, error, name, label, multiple = false, options}: { register: any, name: string, label: string, multiple?: boolean, options: Option[], error: string | undefined }) => (
-    <div className="mb-4 ">
+const SelectField = ({register, error, name, label, multiple = false, options, className, placeholder}: {
+    register: any,
+    name: string,
+    label: string,
+    multiple?: boolean,
+    options: Option[],
+    error: string | undefined,
+    className?: string,
+    placeholder?: string
+}) => (
+    <div className={`${className} mb-4`}>
         <label htmlFor={name} className="block mb-2 text-sm font-medium text-white">{label}</label>
         <select    {...register}
-                multiple={multiple}
-                className="bg-gray-50 border border-gray-300 text-primary text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                   multiple={multiple}
+                   className="bg-gray-50 border border-gray-300 text-primary text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        >
+            {placeholder && <option value='' disabled selected>{placeholder}</option>}
             {options.map((option) => (
                 <option key={option.id} value={option.id}>{option.label}</option>
             ))}
