4abda3c8b832862ef049af67f60a66331cd0654c
Aurélie GRAMUSSET <gramusset.a@gmail.com>
2025-02-01 18:55:39 +0100

    Create pages for movie theater list and single theater

--- pwa/request/movieTheater.ts
+++ pwa/request/movieTheater.ts
@@ -1,4 +1,19 @@
+import Cookies from "js-cookie";
 
+export const fetchMovieTheaterById = async (id: string): Promise<MovieTheater> => {
+    const url = `/movie_theaters/${id}`;
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/ld+json',
+        }
+    });
+    if (!response.ok) {
+        throw new Error('Erreur lors de la récupération de la salle')
+    }
+
+    return await response.json();
+}
 
 export const fetchMovieTheatersByCinema = async (cinema: string): Promise<MovieTheater> => {
     const url = `/movie_theaters?cinema=${cinema}`;
@@ -14,4 +29,20 @@ export const fetchMovieTheatersByCinema = async (cinema: string): Promise<MovieT
     const data = await response.json();
 
     return data['hydra:member'] || [];
+}
+
+export const fetchMovieTheaters = async (page: number, itemsPerPage: number): Promise<MovieTheaterApiResponse> => {
+    const url = `/movie_theaters?page=${page}&itemsPerPage=${itemsPerPage}`;
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/ld+json',
+        }
+    });
+    if (!response.ok) {
+        throw new Error('Erreur lors de la récupération des salles');
+    }
+
+    return await response.json();
+}
 }
