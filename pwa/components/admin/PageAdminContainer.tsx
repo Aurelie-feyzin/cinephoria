e51238fc0f8b0ffc7844dad52ba0d00a11dba38d
Aurélie GRAMUSSET <gramusset.a@gmail.com>
2025-02-02 16:54:49 +0100

    Create: Admin section homepage

---
+++ pwa/components/admin/PageAdminContainer.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import NavBarIntranet from "./Layout/NavBarIntranet";
+import Sidebar from "./Layout/SideBarIntranet";
+import Head from "next/head";
+
+type PageContainerProps = {
+    titlePage: string
+    children?: React.ReactNode
+    action?: React.ReactNode
+};
+
+
+const PageIntranetContainer: React.FC<PageContainerProps> = ({titlePage, children, action}) => {
+    return (
+        <>
+            <Head>
+                <title>Admin Cinephoria</title>
+            </Head>
+            <div className="flex h-screen overflow-hidden">
+                <Sidebar/>
+                <main className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+                    <NavBarIntranet/>
+                    <div className="flex items-center justify-between py-3">
+                        <div className="flex-1 text-center text-black p-4 text-3xl">
+                            <h1>{titlePage}</h1>
+                        </div>
+                        <div className="ml-auto pr-4">
+                            {action}
+                        </div>
+                    </div>
+                    <div className="w-full p-4 md:p-6 2xl:p-10">
+                        {children}
+                    </div>
+                </main>
+            </div>
+        </>
+    );
+};
+
+export default PageIntranetContainer;
---
+++ pwa/pages/admin/_app.tsx
@@ -0,0 +1,21 @@
+import "../styles/globals.css"
+import type {AppProps} from "next/app"
+import {DehydratedState, QueryClient, QueryClientProvider} from "react-query"
+import {useState} from "react";
+import {UserProvider} from "../../context/UserContext";
+import Layout from "../../components/common/Layout";
+
+function AdminApp({Component, pageProps}: AppProps<{ dehydratedState: DehydratedState }>) {
+    const [queryClient] = useState(() => new QueryClient());
+    return (
+        <QueryClientProvider client={queryClient}>
+            <UserProvider>
+                    <Layout dehydratedState={pageProps.dehydratedState}>
+                        <Component {...pageProps} />
+                    </Layout>
+            </UserProvider>
+        </QueryClientProvider>
+    )
+}
+
+export default AdminApp
