3270ddb2129746c4b5f138847107b82b44e3de98
Aurélie GRAMUSSET <gramusset.a@gmail.com>
2025-02-04 20:07:13 +0100

    Install and configure MongoDB bundle for Symfony

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -18,4 +18,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     CoopTilleuls\ForgotPasswordBundle\CoopTilleulsForgotPasswordBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
