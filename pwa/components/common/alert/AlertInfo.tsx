3a86bec020ef47ce594c90e4e7eb0d3c4f47a466
Aurélie GRAMUSSET <gramusset.a@gmail.com>
2025-02-16 20:05:50 +0100

    Improve color palette design

--- pwa/components/common/alert/AlertInfo.tsx
+++ pwa/components/common/alert/AlertInfo.tsx
@@ -7,7 +7,7 @@ const AlertInfo = ({visible = false, titleMessage, message, children}: {
     children?: ReactNode
 }) => {
     return (visible &&
-        <div className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3 rounded-md" role="alert">
+        <div className="bg-primary_light border-t border-b border-primary text-custom_brown px-4 py-3 rounded-md" role="alert">
             <p className="font-bold">{titleMessage}</p>
             <p className="text-sm">{message}</p>
             {children}
