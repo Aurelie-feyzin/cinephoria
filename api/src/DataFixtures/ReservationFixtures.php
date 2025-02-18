750aa461a619adf84e5519603f410904734baa62
Aurélie GRAMUSSET <gramusset.a@gmail.com>
2025-02-18 12:33:35 +0100

    Add fixture for Reservation data

---
+++ api/src/DataFixtures/ReservationFixtures.php
@@ -0,0 +1,93 @@
+<?php
+declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use App\Document\Reservation;
+use App\Entity\MovieShow;
+use App\Repository\SeatRepository;
+use App\Repository\UserRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+
+/** @SuppressWarnings(PHPMD.StaticAccess) */
+class ReservationFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function __construct(
+        private readonly SeatRepository $seatRepository,
+        private readonly DocumentManager $documentManager,
+        private readonly UserRepository $userRepository,
+    ) {
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            MovieShowFixtures::class,
+            UserFixtures::class,
+        ];
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr-FR');
+        $basicUsers = $this->userRepository->findByRole('ROLE_USER');
+        $allMovieShows = $manager->getRepository(MovieShow::class)->findAll();
+
+        foreach ($basicUsers as $user) {
+            $nbReservations = $faker->numberBetween(0, 10);
+            for ($i = 0; $i < $nbReservations; ++$i) {
+                $movieShow = $this->getRandomMovieShow($allMovieShows, $faker);
+                $movieTheater = $movieShow->getMovieTheater();
+                $numberOfSeats = $faker->numberBetween(1, 5);
+                $reservation = (new Reservation())
+                    ->setUserId($user->getId())
+                    ->setMovieId($movieShow->getMovie()->getId())
+                    ->setMovieName($movieShow->getMovie()->getTitle())
+                    ->setMovieBackdropPath($movieShow->getMovie()->getBackdropPath())
+                    ->setCinemaId($movieTheater->getCinema()->getId())
+                    ->setCinemaName($movieTheater->getCinema()->getName())
+                    ->setMovieShowId($movieShow->getId())
+                    ->setMovieShowDate($movieShow->getDate())
+                    ->setMovieShowStartTime($movieShow->getStartTime())
+                    ->setMovieShowEndTime($movieShow->getEndTime())
+                    ->setMovieTheaterId($movieTheater->getId())
+                    ->setMovieTheaterName($movieTheater->getTheaterName())
+                    ->setNumberOfSeats($numberOfSeats)
+                ;
+
+                $this->addUnassignedSeats($numberOfSeats, $reservation, $movieShow->getId());
+
+                $this->documentManager->persist($reservation);
+            }
+        }
+        $this->documentManager->flush();
+    }
+
+    private function addUnassignedSeats(int $unassignedSeats, Reservation $reservation, string $movieShowId): void
+    {
+        $availableSeats = $this->seatRepository->findAvailableSeatsForMovieShow($movieShowId);
+        $nbAvailableSeats = count($availableSeats);
+        for ($i = 0; $i < $unassignedSeats; ++$i) {
+            $reservation->addSeatName($availableSeats[$nbAvailableSeats - 1 - $i]->getName());
+            $reservation->addSeatId($availableSeats[$nbAvailableSeats - 1 - $i]->getId());
+        }
+    }
+
+    /**
+     * @param MovieShow[] $allMovieShow
+     */
+    private function getRandomMovieShow(array $allMovieShow, Generator $faker): MovieShow
+    {
+        $hasAvailableSeats = function ($movieShow) {
+            return $this->seatRepository->countAvailableSeatsForMovieShow($movieShow->getId()) > 5;
+        };
+
+        /* @var MovieShow $movieShow */
+        return $faker->valid($hasAvailableSeats)->randomElement($allMovieShow);
+    }
+}
